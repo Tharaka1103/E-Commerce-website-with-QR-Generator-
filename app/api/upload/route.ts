c00a3cb6c177660c141bc122a014cf93f1193309
Tharaka1103 <supuntharakapro999@gmail.com>
2025-04-14 06:30:26 +0530

    initial ommit

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { writeFile } from 'fs/promises';
+import { writeFile, unlink } from 'fs/promises';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
